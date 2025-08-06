124c88ab21588eb075575b6fe51cd8acd56391d5
Chiwithy <gs4var.avila@gmail.com>
2025-08-06 15:50:18 +0800

    added displayShipInfo and ShipCaller, resolved minor missing code (semicolon, constructors, etc.)

--- ShipWorker.java
+++ ShipWorker.java
@@ -4,6 +4,14 @@ public class ShipWorker {
     private double speedKnots;
     private int fuelLevelPercentage;
 
+    public ShipWorker () {}
+
+    public ShipWorker (String shipName, String captainName, double speedKnots, int fuelLevelPercentage) {
+        this.shipName = shipName;
+        this.captainName = captainName;
+        this.speedKnots = speedKnots;
+        this.fuelLevelPercentage = fuelLevelPercentage;
+    }
     // setter
     public void setShipName(String shipName) {
         this.shipName = shipName;
@@ -34,6 +42,13 @@ public class ShipWorker {
 
     public int getFuelLevelPercentage() {
         return this.fuelLevelPercentage;
+    }
+
+    public void displayShipInfo () {
+        System.out.println ("Ship name: " + getShipName () + "=============");
+        System.out.println ("Ship captain: " + getCaptainName () + "=============");
+        System.out.println ("Ship Speed: " + getSpeedKnots () + " knots");
+        System.out.println ("Ship Fuel: " + getFuelLevelPercentage ()+ "%");
     }
     
 }

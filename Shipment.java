b1e69ea170489820346d97a1a3d7c1bbb100eba6
JAN RUSSEL DERPO (SHPSM-LOSCP-ISD-OOCLL/MNL) <DERPOJA@OOCL.COM>
2025-08-06 14:53:12 +0800

    DERPOJA - created method for Shipment

---
+++ Shipment.java
@@ -0,0 +1,40 @@
+public class Shipment {
+    private String cargoType;
+    private long trackingNumber;
+    private String currentLocation;
+    private int estimatedDeliveryDays;
+    private boolean isDelivered;
+    
+    public String getCargoType() {
+        return cargoType;
+    }
+    public void setCargoType(String cargoType) {
+        this.cargoType = cargoType;
+    }
+    public long getTrackingNumber() {
+        return trackingNumber;
+    }
+    public void setTrackingNumber(long trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+    public int getEstimatedDeliveryDays() {
+        return estimatedDeliveryDays;
+    }
+    public void setEstimatedDeliveryDays(int estimatedDeliveryDays) {
+        this.estimatedDeliveryDays = estimatedDeliveryDays;
+    }
+    public boolean isDelivered() {
+        return isDelivered;
+    }
+    public void setDelivered(boolean isDelivered) {
+        this.isDelivered = isDelivered;
+    }
+
+    
+}

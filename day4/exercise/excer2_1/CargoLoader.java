773341a5277727c49daa758e77447205d12b9633
bee <abigail.manoguid.lia@gmail.com>
2025-08-10 00:40:53 +0800

    MANOGAB: Exer2_1 Done

--- day4/exercise/excer2_1/CargoLoader.java
+++ day4/exercise/excer2_1/CargoLoader.java
@@ -1,13 +1,41 @@
 public class CargoLoader {
 
-    public void loadCargo(double currentWeight, double cargoWeight, double shipCapacity)
+    public void loadCargo (double currentWeight, double cargoWeight, double shipCapacity)
         throws ShipOverloadException, IllegalArgumentException {
         // Validation and loading logic
+
+        if(cargoWeight <= 0) {
+            throw new IllegalArgumentException("Cargo Weight cannot be negative or 0.");
+        }
+        if((currentWeight + cargoWeight) > shipCapacity) {
+            throw new ShipOverloadException("Current weight and Cargo Weight must not exceed Ship Capacity when added.");
+        }
+        System.out.println("Cargo has been loaded successfully");
     }
 
     public static void main(String[] args) {
         CargoLoader loader = new CargoLoader();
+        try {
+            // Test cases here
+            // ShipOverloadException
+            //loader.loadCargo(1500, 600, 2000);
 
-        // Test cases here
+            // IllegalArgumentException
+            //loader.loadCargo(1500, -15, 2000);
+
+            // IllegalArgumentException
+            //loader.loadCargo(1500, 0, 2000);
+
+            // Happy Path
+            loader.loadCargo(1500, 300, 2000);
+            
+        } catch(IllegalArgumentException e) {
+            System.err.println("Exception occured: "+e.getMessage());
+        } catch(ShipOverloadException e) {
+            System.err.println("Exception occured: "+e.getMessage());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }

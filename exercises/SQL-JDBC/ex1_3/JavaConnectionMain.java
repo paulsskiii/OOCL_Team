fdfea5d07a4ecfb83d2fcebfa42dfe1bcd6d050f
Chiwithy <gs4var.avila@gmail.com>
2025-08-12 21:37:19 +0800

    fixed jdbc driver for bare java app (not spring)

--- exercises/SQL-JDBC/ex1_3/JavaConnectionMain.java
+++ exercises/SQL-JDBC/ex1_3/JavaConnectionMain.java
@@ -1,8 +1,16 @@
-
-
 public class JavaConnectionMain {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         DBConnection.getConnection ();
+		
+		System.out.println ("\nStatement ==============");
+		DBConnection.getStatement ();
+
+		System.out.println ("\nGet Prepared ==========");
+		DBConnection.getPrep(67);
+
+		System.out.println ("\nGet Callable ==========");
+		DBConnection.getCall(67);
     }
+
 }

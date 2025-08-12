31275357d13fcff148fa8ae906932575d1962e37
Rh Cali <112600896+RhCali@users.noreply.github.com>
2025-08-12 08:19:08 +0800

    Delete day3 directory

--- day3/excer2/ContainerShip.java
+++
@@ -1,8 +0,0 @@
-public class ContainerShip extends ShipRepository {
-    private int numberOfTEU;
-
-    public ContainerShip(String shipName, String captainName, double speedKnots, int fuelLevelPercentage, int numberOfTEU) {
-        super(shipName, captainName, speedKnots, fuelLevelPercentage);
-        this.numberOfTEU = numberOfTEU;
-    }
-}
--- day3/excer2/TestVessels.java
+++
@@ -1,8 +0,0 @@
-public class TestVessels {
-    public static void main(String[] args) {
-        // Starting code for testing ships
-        ShipRepository genericShip = new ShipRepository("The Wanderer", "Alice Smith", 15.0, 75);
-        Tanker oilTanker = new Tanker("Ocean Giant", "Bob Johnson", 12.5, 90, "Crude Oil");
-        ContainerShip maerskTripleE = new ContainerShip("Maersk Mc-Kinney Møller", "Charlie Brown", 20.0, 80, 18000);
-    }
-}

f5666aa38902df436c29065fb6b2bd703d39741f
sebastian <sebastianicarus52@gmail.com>
2025-08-14 15:26:12 +0800

    miniproject_g4w2_day9

---
+++ miniproject_day9/w2g4/src/main/java/com/w2g4/w2g4/W2g4Application.java
@@ -0,0 +1,13 @@
+package com.w2g4.w2g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2g4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W2g4Application.class, args);
+	}
+
+}
---
+++ miniproject_day9/w2g4/src/test/java/com/w2g4/w2g4/W2g4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2g4.w2g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2g4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

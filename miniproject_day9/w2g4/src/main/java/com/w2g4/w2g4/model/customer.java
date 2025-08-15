05bccfce77f7f598365674abadb4af4086d817fb
sebastian <sebastianicarus52@gmail.com>
2025-08-15 15:29:26 +0800

    updated customer model

--- miniproject_day9/w2g4/src/main/java/com/w2g4/w2g4/model/customer.java
+++ miniproject_day9/w2g4/src/main/java/com/w2g4/w2g4/model/customer.java
@@ -1,9 +1,15 @@
 package com.w2g4.w2g4.model;
 
+import java.security.Timestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -12,17 +18,34 @@ import lombok.AllArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="customer")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
     private Long customer_id;
+
+    @Column(name = "first_name")
     private String first_name;
+
+    @Column(name = "last_name")
     private String last_name;
+
+    @Column(name = "contact_number")
     private String contact_number;
+
+    @Column(name = "email_address")
     private String email_address;
-    private int address_id;
-    private String created_at;
-    private String updated_at;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address_id;
+
+    @Column(name = "created_at")
+    private Timestamp created_at;
+
+    @Column(name = "updated_at")
+    private Timestamp updated_at;
 }
 
 

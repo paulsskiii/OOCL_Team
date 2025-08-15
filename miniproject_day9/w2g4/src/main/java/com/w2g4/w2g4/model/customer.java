b6be3f3f49a60f19e0927a4b00d3b2b7760d89db
VILLABR <VILLABR@OOCL.COM>
2025-08-15 15:36:59 +0800

    Pushed stable version for FE integration

--- miniproject_day9/w2g4/src/main/java/com/w2g4/w2g4/model/customer.java
+++ miniproject_day9/w2g4/src/main/java/com/w2g4/w2g4/model/customer.java
@@ -10,9 +10,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Data

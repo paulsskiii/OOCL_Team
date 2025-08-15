506008e717e4940b9e908354e7eee00fbc2546fd
KATIGCY <cyrusckatigbac@gmail.com>
2025-08-15 08:28:36 +0800

    Initial backend commit

---
+++ Group2MiniProj/backend/src/main/java/com/backend_spring/jdbc_api/JdbcApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend_spring.jdbc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApiApplication.class, args);
+	}
+
+}

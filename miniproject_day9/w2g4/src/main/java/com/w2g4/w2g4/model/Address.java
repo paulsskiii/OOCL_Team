2cccbd2bf0fcb0a8c3feb0b6711ff08bd6c343a0
RAFAEL CHRISTIAN SANCHEZ (EQMI-LOSCP-ISD-OOCLL/MNL) <SANCHRA@OOCL.COM>
2025-08-15 08:36:38 +0800

    Add Address Model

--- miniproject_day9/w2g4/src/main/java/com/w2g4/w2g4/model/Address.java
+++ miniproject_day9/w2g4/src/main/java/com/w2g4/w2g4/model/Address.java
@@ -4,32 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class City {
+public class Address {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int city_id;
-    private String name;
+    private Long address_id; // Primary Key
+    private String address_line1;
+    private String address_line2;
+    private String barangay;
+    private City city; // Foreign Key to City Table
+    private Integer postal_code;
+    private String region;
+    private String country;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

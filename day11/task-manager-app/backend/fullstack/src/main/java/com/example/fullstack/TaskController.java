ea6f86b01e35b017235ed56ae6454bcaccdf877a
ANGELO JAMES NUESTRO (GEDI-B2BGEDI-ISD-OOCLL/MNL) <NUESTAN@OOCL.COM>
2025-08-18 21:33:07 +0800

    added updated filter & search backend functions in project dir

---
+++ day11/task-manager-app/backend/fullstack/src/main/java/com/example/fullstack/TaskController.java
@@ -0,0 +1,60 @@
+package com.example.fullstack;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin (origins = "http://localhost:3000")
+@RestController
+@RequestMapping ("/api/tasks")
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    @GetMapping("/search")
+    public List<Task> searchTask(@RequestParam String input){
+        return taskRepository.findByTitleContainingOrDescriptionContainingIgnoreCase(input, input);
+    }
+    
+    @GetMapping("/filter/{status}")
+    public List<Task> filterByStatus(@PathVariable boolean status){
+        if(status){
+            return taskRepository.findByCompletedTrue();
+        } else {
+            return taskRepository.findByCompletedFalse();
+        }
+        
+    }
+
+}
---
+++ day11/task-manager-app/backend/fullstack/src/main/java/com/example/fullstack/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.example.fullstack;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByTitleContainingOrDescriptionContainingIgnoreCase(String title, String description);
+
+    List<Task> findByCompletedTrue();
+
+    List<Task> findByCompletedFalse();
+}

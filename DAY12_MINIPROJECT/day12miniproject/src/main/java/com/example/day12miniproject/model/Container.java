c70a7421c0f3d6e0601e55e75030d62dd31d93f8
Sebastian Icarus <sebastianicarus52@gmail.com>
2025-08-19 14:18:55 +0800

    fix container model

--- DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/model/Container.java
+++ DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/model/Container.java
@@ -1,9 +1,11 @@
 package com.example.day12miniproject.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,13 +14,23 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="container")
 public class Container {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id")
     private Long id;
-    private String name;
+
+    @Column(name ="container_name")
+    private String containerName;
+
+    @Column(name ="weight")
     private double weight; // in kg
+
+    @Column(name ="origin")
     private String origin;
+
+    @Column(name ="destination")
     private String destination;
 
 }

2a3820e636b5f2728ae8ba6b6fd757e6caa3345f
annadese <annapat.desembrana@gmail.com>
2025-08-19 23:47:27 +0800

    Added day10 mini-project-1

---
+++ day10/mini-project-1/src/main/java/ita/bootcamp/miniproject/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package ita.bootcamp.miniproject.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ita.bootcamp.miniproject.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    List<Customer> findByFirstNameContainingIgnoreCase(String firstName);
+    List<Customer> findByLastNameContainingIgnoreCase(String lastName);
+    List<Customer> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
+
+}

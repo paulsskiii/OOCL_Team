538492558b33fe78bb2c3a1c2e72b499ee546804
ANGELO JAMES NUESTRO (GEDI-B2BGEDI-ISD-OOCLL/MNL) <NUESTAN@OOCL.COM>
2025-08-19 11:34:40 +0800

    personal detaills model, controller, repository

---
+++ day12/exercise1/day12/src/main/java/com/example/day12/PersonalDetailsRepository.java
@@ -0,0 +1,10 @@
+package com.example.day12;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonalDetailsRepository  extends JpaRepository<PersonalDetails, Long>{
+
+    
+} 

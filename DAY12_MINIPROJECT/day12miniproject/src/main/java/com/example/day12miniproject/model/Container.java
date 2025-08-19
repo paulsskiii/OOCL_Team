2034689a680424894451f3b08c6db175b3364b12
Sebastian Icarus <sebastianicarus52@gmail.com>
2025-08-19 13:33:54 +0800

    added container model

--- DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/model/Container.java
+++ DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/model/Container.java
@@ -1,5 +1,24 @@
 package com.example.day12miniproject.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Container {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double weight; // in kg
+    private String origin;
+    private String destination;
+
 }

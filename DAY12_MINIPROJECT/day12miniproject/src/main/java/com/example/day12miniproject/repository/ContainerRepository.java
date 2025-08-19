7c1249ed043c5152b01f247eb217cf2b1fbf0ac2
Sebastian Icarus <sebastianicarus52@gmail.com>
2025-08-19 13:39:51 +0800

    added Container Repository

--- DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/repository/ContainerRepository.java
+++ DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/repository/ContainerRepository.java
@@ -1,5 +1,11 @@
 package com.example.day12miniproject.repository;
 
-public class ContainerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.day12miniproject.model.Container;
+
+@Repository
+public interface ContainerRepository extends JpaRepository<Container, Long> {
     
 }

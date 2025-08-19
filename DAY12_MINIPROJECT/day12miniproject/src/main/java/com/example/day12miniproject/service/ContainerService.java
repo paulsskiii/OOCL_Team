09d746240c9fb46fdd487989b359384ede9b815d
RAFAEL CHRISTIAN SANCHEZ (EQMI-LOSCP-ISD-OOCLL/MNL) <SANCHRA@OOCL.COM>
2025-08-19 13:56:11 +0800

    Add ContainerService

--- DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/service/ContainerService.java
+++ DAY12_MINIPROJECT/day12miniproject/src/main/java/com/example/day12miniproject/service/ContainerService.java
@@ -1,5 +1,48 @@
 package com.example.day12miniproject.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.day12miniproject.model.Container;
+import com.example.day12miniproject.repository.ContainerRepository;
+
 public class ContainerService {
+
+    private final  ContainerRepository containerRepository;
+
+      public ContainerService(ContainerRepository containerRepository) {
+        this.containerRepository = containerRepository;
+    }
+
+    public List<Container> getAllContainer() {
+        return containerRepository.findAll(); // Uses JpaRepository's findAll()
+    }
+
+    public Optional<Container> getContainerById(Long id) {
+        return containerRepository.findById(id); // Uses JpaRepository's findById()
+    }
+
+    public Container addContainers(Container container) {
+        return containerRepository.save(container); // Uses JpaRepository's save() for create/update
+    }
+
+    public void deleteContainer(Long id) {
+        containerRepository.deleteById(id); // Uses JpaRepository's deleteById()
+    }
     
+    public int countbyDestination (Long id, String destination) {
+        List<Container> containerList = containerRepository.findAll();
+
+        return containerList.stream()
+            .filter(container -> container.getDestination.toUpperCase() == destination.toUpperCase())
+            .count();
+    }
+
+    public int countbyOrigin (Long id, String origin) {
+        List<Container> containerList = containerRepository.findAll();
+
+        return containerList.stream()
+            .filter(container -> container.getOrigin.toUpperCase() == origin.toUpperCase())
+            .count();
+    }
 }

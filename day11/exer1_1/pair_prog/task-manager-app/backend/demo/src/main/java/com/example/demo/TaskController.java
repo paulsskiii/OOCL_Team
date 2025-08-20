d9410df686352c8db2a68df36a0e4c30985b05e8
MONIQUE AIRA DESENGANO (DOCSM-LOSCP-ISD-OOCLL/MNL) <DESENMO2@OOCL.COM>
2025-08-20 08:58:54 +0800

    Completed Day 11 Pair Prog Activity

---
+++ day11/exer1_1/pair_prog/task-manager-app/backend/demo/src/main/java/com/example/demo/TaskController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}

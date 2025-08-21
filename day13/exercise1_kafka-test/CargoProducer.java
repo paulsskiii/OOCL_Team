8b6009a132a302f1f5ee03fdb95fcfd5014fdb3f
arellma <martinjamesarellano@gmail.com>
2025-08-21 16:26:37 +0800

    Added exercises for Day 11 (pair programming) and for Day 12 (kafka test)

---
+++ day13/exercise1_kafka-test/CargoProducer.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.apache.kafka.clients.producer.*; 
+import java.util.Properties; 
+import java.time.Instant; 
+ 
+public class CargoProducer { 
+    public static void main(String[] args) { 
+        Properties props = new Properties(); 
+        props.put("bootstrap.servers", "localhost:9092"); 
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
+ 
+        try (KafkaProducer<String, String> producer = new KafkaProducer<>(props)) { 
+            
+            for (int i = 0; i < 5; i++) { 
+
+                for(int j = 0; j < 10; j++){
+                    String cargoId = "C-10" + j; 
+                    String status = "IN_TRANSIT-" + i; 
+                    String message = String.format("{\"cargo_id\":\"%s\", \"status\":\"%s\", \"timestamp\":\"%s\"}",  
+                        cargoId, status, Instant.now().toString()); 
+                    
+
+                    // ProducerRecord with topic, key, and value 
+                    ProducerRecord<String, String> record1 = new ProducerRecord<>("cargo-events-new", cargoId, message); 
+
+                    producer.send(record1, (metadata, exception) -> { 
+                        if (exception == null) { 
+                            System.out.printf("Sent record to topic %s, partition %d, offset %d%n", 
+                                    metadata.topic(), metadata.partition(), metadata.offset()); 
+                        } else { 
+                            exception.printStackTrace(); 
+                        } 
+                    }); 
+                }
+            } 
+            producer.flush(); // Ensure all messages are sent before closing 
+        } 
+        }
+    
+}

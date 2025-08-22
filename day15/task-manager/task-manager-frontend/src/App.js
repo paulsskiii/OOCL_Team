7a67bd9079d62a10a6b341861a4e64ba261f47e9
Chiwithy <gs4var.avila@gmail.com>
2025-08-23 02:43:24 +0800

    added podman taskmanager exercise; tested working w/ podman on ITA PC

---
+++ day15/task-manager/task-manager-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TaskList from "./components/TaskList";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Task Manager</h1>
+        <p>Full-stack application with Spring Boot and React</p>
+      </header>
+      <main>
+        <TaskList />
+      </main>
+    </div>
+  );
+}
+export default App;
---
+++ day15/task-manager/task-manager-frontend/src/services/taskService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const API_BASE_URL = "http://localhost:8080/api";
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+export const taskService = {
+  getAllTasks: () => api.get("/tasks"),
+  getTaskById: (id) => api.get(`/tasks/${id}`),
+  createTask: (task) => api.post("/tasks", task),
+  updateTask: (id, task) => api.put(`/tasks/${id}`, task),
+  deleteTask: (id) => api.delete(`/tasks/${id}`),
+};

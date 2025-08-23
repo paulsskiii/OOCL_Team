c3b5ed0e56a116d0fdc4d583bd417a61f8cfdce4
ALEXIS CAO (DOCSM-LOSCP-ISD-OOCLL/MNL) <CAOAL4@OOCL.COM>
2025-08-23 21:52:57 +0800

    Completed Day 12 and Day 13 Activities

---
+++ CAOAL4-ACTIVITIES/Day12/Group-Activity/exercise2/excer2/src/main/java/com/example/excer2/model/Container.java
@@ -0,0 +1,19 @@
+package com.example.excer2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Container {
+
+    @Id
+    private String containerNumber;
+    private String origin;
+    private String destination;
+    private double weightKg;
+
+}
---
+++ CAOAL4-ACTIVITIES/Day13/kafka/src/main/java/com/example/kafka/CargoProducer4.java
@@ -0,0 +1,52 @@
+package com.example.kafka;
+
+import java.time.Instant;
+import java.util.Properties;
+ 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+ 
+public class CargoProducer4 { 
+    public static void main(String[] args) { 
+        Properties props = new Properties(); 
+        props.put("bootstrap.servers", "localhost:9092"); 
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
+ 
+        try (KafkaProducer<String, String> producer = new KafkaProducer<>(props)) { 
+            for (int i = 0; i < 50; i++) { 
+                String cargoId1 = "C-101"; 
+                String cargoId2 = "C-102"; 
+                String status1 = "IN_TRANSIT-" + i; 
+                String status2 = "DELIVERED-" + i;
+                String message1 = String.format("{\"cargo_id\":\"%s\", \"status\":\"%s\", \"timestamp\":\"%s\"}",  
+                    cargoId1, status1, Instant.now().toString()); 
+                String message2 = String.format("{\"cargo_id\":\"%s\", \"status\":\"%s\", \"timestamp\":\"%s\"}",  
+                    cargoId2, status2, Instant.now().toString());
+ 
+                //USE CARGO_ID AS THE KEY
+                ProducerRecord<String,String> record1 = new ProducerRecord<>("cargo-events", cargoId1, message1);
+                ProducerRecord<String,String> record2 = new ProducerRecord<>("cargo-events", cargoId2, message2);
+
+ 
+                producer.send(record1, (metadata, exception) -> { 
+                    if (exception == null) { 
+                        System.out.printf("Sent record to topic %s, partition %d, offset %d%n", 
+                                metadata.topic(), metadata.partition(), metadata.offset()); 
+                    } else { 
+                        exception.printStackTrace(); 
+                    } 
+                });
+                producer.send(record2, (metadata, exception) -> { 
+                    if (exception == null) { 
+                        System.out.printf("Sent record to topic %s, partition %d, offset %d%n", 
+                                metadata.topic(), metadata.partition(), metadata.offset()); 
+                    } else { 
+                        exception.printStackTrace(); 
+                    } 
+                }); 
+            } 
+            producer.flush(); // Ensure all messages are sent before closing 
+        } 
+    } 
+}

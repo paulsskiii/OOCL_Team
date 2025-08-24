f862eadade69c171e6746d9d5604e5e05e5adfdd
Philip Ronin Uriarte <philipuriarte@gmail.com>
2025-08-24 11:31:06 +0800

    Initial commit: Spring Boot + React task manager

---
+++ day14/task-manager/src/main/java/com/workshop/task_manager/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.workshop.task_manager.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.workshop.task_manager.entity.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
+    List<Task> findByCompletedOrderByCreatedAtDesc(Boolean completed);
+} 

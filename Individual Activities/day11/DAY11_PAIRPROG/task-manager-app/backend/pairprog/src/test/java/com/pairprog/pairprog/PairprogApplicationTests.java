dfa50020d5644ef0e9ebabc7a91904edf19ba2a4
Juan Semana <semanju@oocl.com>
2025-08-25 13:51:39 +0800

    Restructure: push day 11-14

---
+++ Individual Activities/day11/DAY11_PAIRPROG/task-manager-app/backend/pairprog/src/test/java/com/pairprog/pairprog/PairprogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pairprog.pairprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairprogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

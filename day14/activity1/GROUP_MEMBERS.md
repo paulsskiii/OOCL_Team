d5ab0a4bcf9b0f183ccdb99f0f2e54f5fba97492
Kurtlo <kurtlonolasco@gmail.com>
2025-08-26 08:28:10 +0800

    Group 4 - Activity 1 for Day 14

---
+++ day14/activity1/GROUP_MEMBERS.md
@@ -0,0 +1,8 @@
+# Group Members
+
+| Name                | Role                | GitHub Username |
+|---------------------|---------------------|-----------------|
+| Russel Kurt Nolasco | Group Leader        | @kurt-lo        |
+| Hikaru Aquino       | Developer           | @Miss-Apple     |
+| Jan Russel Derpo    | Developer  | @janrusselderpo |
+| Paul Barredo        | Developer              | @               |
---
+++ day14/activity1/task-manager/src/main/java/com/workshop/taskmanager/service/TaskService.java
@@ -0,0 +1,41 @@
+package com.workshop.taskmanager.service;
+
+import com.workshop.taskmanager.model.Task;
+import com.workshop.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public List<Task> getTasksByStatus(Boolean completed) {
+        return taskRepository.findByCompletedOrderByCreatedAtDesc(completed);
+    }
+
+}

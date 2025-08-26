555903d001996a0a04ad97ce2a9a216ac78f2a42
KATIGCY <cyrusckatigbac@gmail.com>
2025-08-26 18:28:51 +0800

    Added Day 13 Activity Kafka Producer and Consumer

---
+++ day13/exercise/src/test/java/com/kafka/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

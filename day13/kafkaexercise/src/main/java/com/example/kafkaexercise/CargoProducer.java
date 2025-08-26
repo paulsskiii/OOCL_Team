c8f8b7d0e95b40e86740939ef6cea904fb6627dc
annadese <annapat.desembrana@gmail.com>
2025-08-26 09:39:35 +0800

    Added day13 Kafka exercise 1 for asynchronous processing for cargo

--- day13/kafkaexercise/src/main/java/com/example/kafkaexercise/CargoProducer.java
+++ day13/kafkaexercise/src/main/java/com/example/kafkaexercise/CargoProducer.java
@@ -1,6 +1,10 @@
 package com.example.kafkaexercise;
 
-import org.apache.kafka.clients.producer.*; 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties; 
 import java.time.Instant; 
  
@@ -11,25 +15,29 @@ public class CargoProducer {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
         
+        List<String> cargoIdList = new ArrayList<>();
+        cargoIdList.add("C-101"); 
+        cargoIdList.add("C-102"); 
  
         try (KafkaProducer<String, String> producer = new KafkaProducer<>(props)) { 
             for (int i = 0; i < 5; i++) { 
-                String cargoId = "C-101"; 
                 String status = "IN_TRANSIT-" + i; 
-                String message = String.format("{\"cargo_id\":\"%s\", \"status\":\"%s\", \"timestamp\":\"%s\"}",  
-                    cargoId, status, Instant.now().toString()); 
- 
+            
                 // ProducerRecord with topic, key, and value 
-                ProducerRecord<String, String> record = new ProducerRecord<>("cargo-events", cargoId, message); 
- 
-                producer.send(record, (metadata, exception) -> { 
-                    if (exception == null) { 
-                        System.out.printf("Sent record to topic %s, partition %d, offset %d%n", 
-                                metadata.topic(), metadata.partition(), metadata.offset()); 
-                    } else { 
-                        exception.printStackTrace(); 
-                    } 
-                }); 
+                for(String cargoId : cargoIdList) {
+                    String message = String.format("{\"cargo_id\":\"%s\", \"status\":\"%s\", \"timestamp\":\"%s\"}",  
+                    cargoId, status, Instant.now().toString()); 
+                    ProducerRecord<String, String> record = new ProducerRecord<>("cargo-events", cargoId, message); 
+
+                    producer.send(record, (metadata, exception) -> { 
+                        if (exception == null) { 
+                            System.out.printf("Sent record to topic %s, partition %d, offset %d%n", 
+                                    metadata.topic(), metadata.partition(), metadata.offset()); 
+                        } else { 
+                            exception.printStackTrace(); 
+                        } 
+                    }); 
+                }
             } 
             producer.flush(); // Ensure all messages are sent before closing 
         } 

e8ca944e05b0c09103c242c561f9d9ca3240ff09
Koki <asdale0122@gmail.com>
2025-08-27 14:59:52 +0800

    Added day16 exercises

---
+++ day15/Testing/src/test/java/com/example/testing/OrderServiceIntegrationTest.java
@@ -0,0 +1,14 @@
+// // --- OrderServiceIntegrationTest.java ---
+// @SpringBootTest
+// @AutoConfigureWireMock(port = 8081)
+// public class OrderServiceIntegrationTest {
+//     @Autowired private OrderService orderService;
+//     @Test
+//     void testProcessPaymentSuccess() {
+//         stubFor(post(urlEqualTo("/payments/process"))
+//                 .willReturn(aResponse().withStatus(200).withBody("{ \"status\": \"SUCCESS\" }")));
+//         Order order = new Order("Laptop", 999.99);
+//         String paymentStatus = orderService.processPayment(order);
+//         assertThat(paymentStatus).isEqualTo("SUCCESS");
+//     }
+// }

ef4dd57f9c5e7d6ab791d6d77268f8b6becd45f1
micahfabon <fabon.work@gmail.com>
2025-08-27 09:38:38 +0800

    added day 4 exercise 2.1 answers

--- day4/exercise/excer2_1/CargoLoader.java
+++ day4/exercise/excer2_1/CargoLoader.java
@@ -2,12 +2,27 @@ public class CargoLoader {
 
     public void loadCargo(double currentWeight, double cargoWeight, double shipCapacity)
         throws ShipOverloadException, IllegalArgumentException {
-        // Validation and loading logic
+            double totalWeight = currentWeight + cargoWeight;
+
+            if (cargoWeight <= 0) {
+                throw new IllegalArgumentException("Cargo weight should be greater than 0");
+            }
+            if (totalWeight > shipCapacity) {
+                throw new ShipOverloadException("Total Weight exceeds Ship's Capacity");
+            }
+            System.out.println("Total Weight: " + totalWeight);
+          
     }
 
     public static void main(String[] args) {
         CargoLoader loader = new CargoLoader();
-
-        // Test cases here
+        try {
+            System.out.println("=== Ship 1 ===");
+            loader.loadCargo(2, 1, 4);
+            System.out.println("=== Ship 2 ===");
+            loader.loadCargo(2, 7, 4);
+        } catch (ShipOverloadException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }

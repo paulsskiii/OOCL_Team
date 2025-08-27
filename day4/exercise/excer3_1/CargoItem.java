62237b45108255a0d099c7fe10f8aa2fc9f8fa30
micahfabon <fabon.work@gmail.com>
2025-08-27 09:49:51 +0800

    added day 4 exercise 3.1 answers

--- day4/exercise/excer3_1/CargoItem.java
+++ day4/exercise/excer3_1/CargoItem.java
@@ -11,17 +11,12 @@ class CargoItem {
         this.value = value;
     }
 
-    // Getters and toString
+    @Override
+    public String toString() {
+        return "=== Cargo ID " + this.id + " ==="
+                            + "\nName: " + this.name 
+                            + "\nWeight: " + this.weight
+                            + "\nValue: " + this.value + "\n";
+    }
+    
 }
-
-
-
-// // Vars
-// CargoItem(201, "Gold Bars", 100.0, 500000.0)
-// CargoItem(202, "Luxury Cars", 5000.0, 150000.0)
-// CargoItem(203, "General Goods", 2000.0, 8000.0)
-// CargoItem(204, "Art Collection", 50.0, 25000.0)
-// CargoItem(205, "Building Materials", 10000.0, 5000.0)
-// CargoItem(206, "Rare Manuscripts", 10.0, 12000.0)
-
-// threshold = 10000.0
--- day4/exercise/excer3_1/HighValueCargoFilter.java
+++ day4/exercise/excer3_1/HighValueCargoFilter.java
@@ -5,11 +5,25 @@ import java.util.stream.Collectors;
 public class HighValueCargoFilter {
     public static void main(String[] args) {
         List<CargoItem> allCargo = Arrays.asList(
-            // Cargo items
+            new CargoItem(201, "Gold Bars", 100.0, 500000.0),
+            new CargoItem(202, "Luxury Cars", 5000.0, 150000.0),
+            new CargoItem(203, "General Goods", 2000.0, 8000.0),
+            new CargoItem(204, "Art Collection", 50.0, 25000.0),
+            new CargoItem(205, "Building Materials", 10000.0, 5000.0),
+            new CargoItem(206, "Rare Manuscripts", 10.0, 12000.0)
         );
 
         double threshold = 10000.0;
 
         // Filter and display logic
+
+        List<CargoItem> filteredCargo = allCargo.stream()
+                                                .filter(cargo -> cargo.value > threshold)
+                                                .collect(Collectors.toList());
+
+        for (CargoItem cargo : filteredCargo) {
+            System.out.println(cargo.toString());
+        }
+        
     }
 }

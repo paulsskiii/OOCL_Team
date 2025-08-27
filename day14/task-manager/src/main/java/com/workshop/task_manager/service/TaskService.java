5be3076f3c4679bc0f9632dcaab60939abfc4a9c
URIARPH <philiproninu@gmail.com>
2025-08-27 17:50:07 +0800

    Add day 14 exercise

---
+++ day14/task-manager/src/main/java/com/workshop/task_manager/service/TaskService.java
@@ -0,0 +1,40 @@
+package com.workshop.task_manager.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshop.task_manager.entity.Task;
+import com.workshop.task_manager.repository.TaskRepository;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public List<Task> getTasksByStatus(Boolean completed) {
+        return taskRepository.findByCompletedOrderByCreatedAtDesc(completed);
+    }
+}

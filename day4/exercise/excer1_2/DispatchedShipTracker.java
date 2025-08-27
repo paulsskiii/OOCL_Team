5cc39aa502959c147ab05d9eb736725b2526f651
micahfabon <fabon.work@gmail.com>
2025-08-27 09:29:51 +0800

    added day 4 exercise 1.2 answers

--- day4/exercise/excer1_2/DispatchedShipTracker.java
+++ day4/exercise/excer1_2/DispatchedShipTracker.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class DispatchedShipTracker {
@@ -6,7 +7,22 @@ public class DispatchedShipTracker {
         Set<String> dispatchedShips = new HashSet<>();
 
         // Add ship names
+        dispatchedShips.add("Ship A");
+        dispatchedShips.add("Ship B");
+        dispatchedShips.add("Ship B");
+        dispatchedShips.add("Ship C");
 
         // Check for ship presence
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a vessel name to check if dispatched: ");
+        String vesselName = input.nextLine();
+        input.close();
+
+        System.out.println("Is " + vesselName + " dispatched? " + dispatchedShips.contains(vesselName));
+
+        System.out.println("===== List of Dispatched Ships: =====");
+        for (String dispatchedShip : dispatchedShips) {
+            System.out.println(dispatchedShip);
+        }
     }
 }

786be012254a695d9b9f9f034e69783973ed5c91
RAFAEL CHRISTIAN SANCHEZ (EQMI-LOSCP-ISD-OOCLL/MNL) <SANCHRA@OOCL.COM>
2025-08-27 09:07:56 +0800

    Added day15 Group Activity for JUnit Testing

---
+++ day15/day15miniproject/day15miniproject/src/main/java/com/example/day12miniproject/service/ContainerService.java
@@ -0,0 +1,52 @@
+package com.example.day12miniproject.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.day12miniproject.model.Container;
+import com.example.day12miniproject.repository.ContainerRepository;
+
+@Service
+public class ContainerService {
+    @Autowired
+    private final  ContainerRepository containerRepository;
+
+      public ContainerService(ContainerRepository containerRepository) {
+        this.containerRepository = containerRepository;
+    }
+
+    public List<Container> getAllContainer() {
+        return containerRepository.findAll(); // Uses JpaRepository's findAll()
+    }
+
+    public Optional<Container> getContainerById(Long id) {
+        return containerRepository.findById(id); // Uses JpaRepository's findById()
+    }
+
+    public Container addContainers(Container container) {
+        return containerRepository.save(container); // Uses JpaRepository's save() for create/update
+    }
+
+    public void deleteContainer(Long id) {
+        containerRepository.deleteById(id); // Uses JpaRepository's deleteById()
+    }
+    
+    public Long countbyDestination (Long id, String destination) {
+        List<Container> containerList = containerRepository.findAll();
+
+        return containerList.stream()
+            .filter(container -> container.getDestination().toUpperCase() == destination.toUpperCase())
+            .count();
+    }
+
+    public Long countbyOrigin (Long id, String origin) {
+        List<Container> containerList = containerRepository.findAll();
+
+        return containerList.stream()
+            .filter(container -> container.getOrigin().toUpperCase() == origin.toUpperCase())
+            .count();
+    }
+}

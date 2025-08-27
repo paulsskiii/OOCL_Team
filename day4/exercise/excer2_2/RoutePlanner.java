e62cfc19970f40719ccdf456fb26682c3d81bfb9
micahfabon <fabon.work@gmail.com>
2025-08-27 09:44:15 +0800

    added day 4 exercise 2.2 answers

--- day4/exercise/excer2_2/RoutePlanner.java
+++ day4/exercise/excer2_2/RoutePlanner.java
@@ -5,33 +5,49 @@ public class RoutePlanner {
     private List<String> validPorts = Arrays.asList("Manila", "Singapore", "Tokyo", "Busan", "Hong Kong");
 
     public void planRoute(String origin, String destination) throws PortNotRecognizedException {
-        // Validation logic
+        if (validPorts.contains(origin) && validPorts.contains(destination)) {
+            System.out.println("Valid ports");
+        } else {
+            throw new PortNotRecognizedException("Port/s not recognized");
+        }
     }
 
     public static void main(String[] args) {
         RoutePlanner planner = new RoutePlanner();
+        try {
+            System.out.println("Test Case 1 ");
+            planner.planRoute("Manila", "Singapore");
+        } catch (PortNotRecognizedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println();
+        }
 
-        // Test cases
+        try {
+            System.out.println("Test Case 2");
+            planner.planRoute("Tokyo", "New York");
+        } catch (PortNotRecognizedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println();
+        }
+
+        try {
+            System.out.println("Test Case 3");
+            planner.planRoute("London", "Busan");
+        } catch (PortNotRecognizedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println();
+        }
+
+        try {
+            System.out.println("Test Case 4");
+            planner.planRoute("London", "New York");
+        } catch (PortNotRecognizedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println();
+        }
     }
 }
-
-
-
-
-// // test case
-
-// // Test Case 1
-// origin = "Manila";
-// destination = "Singapore";
-
-// // Test Case 2
-// origin = "Tokyo";
-// destination = "New York";
-
-// // Test Case 3
-// origin = "London";
-// destination = "Busan";
-
-// // Test Case 4
-// origin = "London";
-// destination = "New York";

06d8f907e78037b190b1b0d66fe564cac443acfe
VILLABR <VILLABR@OOCL.COM>
2025-08-28 13:39:48 +0800

    Initial commit; Configured .gitignore and setup project structure

---
+++ capstone/container_management_system/src/main/java/com/g3w4/container_management_system/ContainerManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.g3w4.container_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerManagementSystemApplication.class, args);
+	}
+
+}
---
+++ capstone/container_management_system/src/test/java/com/g3w4/container_management_system/ContainerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3w4.container_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

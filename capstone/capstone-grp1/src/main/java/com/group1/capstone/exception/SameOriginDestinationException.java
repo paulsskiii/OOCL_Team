af7797ccc22d28985cf3013750fec5bfe6cc8512
annadese <annapat.desembrana@gmail.com>
2025-08-28 13:17:30 +0800

    [CSMP-30] Added initial project files from previous mini-project (Container Management Dashboard)

---
+++ capstone/capstone-grp1/src/main/java/com/group1/capstone/exception/SameOriginDestinationException.java
@@ -0,0 +1,7 @@
+package com.group1.capstone.exception;
+
+public class SameOriginDestinationException extends Exception {
+    public SameOriginDestinationException(String message) {
+        super(message);
+    }
+}
---
+++ capstone/capstone-grp1/src/test/java/com/group1/capstone/repository/ContainerRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.group1.capstone.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.group1.capstone.CapstoneApplication;
+import com.group1.capstone.model.Container;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
+
+@DataJpaTest
+@ContextConfiguration(classes = CapstoneApplication.class)
+public class ContainerRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private ContainerRepository containerRepository;
+
+    @Test
+    void given_container_number_when_find_by_container_number_then_should_return_containers() {
+
+        entityManager.persist(new Container("OOOCL1233","Manila","Mindanao",150000.22));
+        Optional <List <Container>> givenContainer = containerRepository.findByContainerNumber("OOOCL1233");
+        assertThat(givenContainer).isPresent();
+    }
+
+    @Test
+    void given_no_container_number_when_find_by_container_number_then_should_return_no_containers() {
+        Optional <List <Container>> givenContainer = containerRepository.findByContainerNumber("OOOCL1233");
+        assertThat(givenContainer).isPresent();
+    }
+
+}

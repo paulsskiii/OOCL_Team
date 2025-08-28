b9afc40407b3ab2f25ca2f7bcfcdf7d3fe3420fa
PRINCESS JOY FERRER (DOCSM-LOSCP-ISD-OOCLL/MNL) <FERREPR2@OOCL.COM>
2025-08-28 16:24:47 +0800

    [CSMP-11] Refactor: removed comments

--- capstone/capstone-grp1/dbscripts/cargo_shipping_schema.sql
+++ capstone/capstone-grp1/dbscripts/cargo_shipping_schema.sql
@@ -2,33 +2,21 @@ CREATE SCHEMA `cargo_shipping`;
 
 USE cargo_shipping;
 
-DROP SCHEMA CARGO_SHIPPING;
-
 CREATE TABLE user (
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+ id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
-    role VARCHAR(50) UNIQUE NOT NULL, -- "USER" / "ADMIN"
+    role VARCHAR(50) UNIQUE NOT NULL, 
     password VARCHAR(50) NOT NULL,
     contact_number VARCHAR(16) UNIQUE NOT NULL,
     created_at DATETIME DEFAULT NOW(),
     updated_at DATETIME   
 );
 
--- CREATE TABLE admin (
--- 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
---     username VARCHAR(50) NOT NULL,
---     email VARCHAR(50) UNIQUE NOT NULL,
---     password VARCHAR(50) NOT NULL,
---     contact_number VARCHAR(16) UNIQUE NOT NULL,
---     created_at DATETIME DEFAULT NOW(),
---     updated_at DATETIME 
--- );
-
 CREATE TABLE port (
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+ id INTEGER PRIMARY KEY AUTO_INCREMENT,
     port_location VARCHAR(100) NOT NULL,
     port_code VARCHAR(50) UNIQUE NOT NULL,
     created_at DATETIME DEFAULT NOW(),
@@ -36,7 +24,7 @@ CREATE TABLE port (
 );
 
 CREATE TABLE status (
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+ id INTEGER PRIMARY KEY AUTO_INCREMENT,
     status_type VARCHAR(100) NOT NULL,
     status_code VARCHAR(50) UNIQUE NOT NULL,
     created_at DATETIME DEFAULT NOW(),
@@ -44,8 +32,8 @@ CREATE TABLE status (
 );
 
 CREATE TABLE cargo (
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
+ id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
     descriptions VARCHAR(254),
     status_id INTEGER NOT NULL,
     destination INTEGER NOT NULL,
@@ -61,17 +49,16 @@ CREATE TABLE cargo (
 );
 
 CREATE TABLE tracking_event(
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+ id INTEGER PRIMARY KEY AUTO_INCREMENT,
     cargo_id INTEGER NOT NULL,
     status_id INTEGER NOT NULL,
     created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME,
     FOREIGN KEY (cargo_id) REFERENCES cargo(id),
     FOREIGN KEY (status_id) REFERENCES status(id)
 );
 
 
 
-
-
-
-
+ 
+ 

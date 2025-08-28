6876b4c818114f244c32ef266294d12d4787c986
Sebastian Icarus <sebastianicarus52@gmail.com>
2025-08-28 13:53:44 +0800

    [CSMP-36] Added repository classes (CargoRepository.java, PortRepository.java, StatusRepository.java, TrackingEventRepository.java, UserRepository.java)

---
+++ capstone/capstone-grp1/src/main/java/com/group1/capstone/repository/TrackingEventRepository.java
@@ -0,0 +1,11 @@
+package com.group1.capstone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.group1.capstone.model.TrackingEvent;
+
+@Repository
+public interface TrackingEventRepository extends JpaRepository<TrackingEvent, Long> {
+    
+}

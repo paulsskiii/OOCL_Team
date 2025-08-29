72c0fddbf23332344d4495f700de4a0c3bb4a000
Chiwithy <gs4var.avila@gmail.com>
2025-08-29 17:00:22 +0800

    [SCRUM-2] created UserInformationRepository method to search by active email findByActiveEmail (String)

--- capstone/cargo-shipping-management-system/src/main/java/com/example/capstoneapplication/repository/UserInformationRepository.java
+++ capstone/cargo-shipping-management-system/src/main/java/com/example/capstoneapplication/repository/UserInformationRepository.java
@@ -3,11 +3,13 @@ package com.example.capstoneapplication.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.capstoneapplication.model.UserInformation;
 
 @Repository
 public interface UserInformationRepository extends JpaRepository<UserInformation, Long>{
-    public Optional<UserInformation> findByEmail (String email);
+    @Query(value="SELECT user_information.* FROM user_information, user_credentials WHERE user_information.email=?1 AND user_information.user_credentials_id=user_credentials.`id` AND user_credentials.is_active=1 LIMIT 1", nativeQuery = true)
+    public Optional<UserInformation> findByActiveEmail (String email);
 }

e5da61e86e98e5d643888de2120d1f3d3aab29e1
Chiwithy <gs4var.avila@gmail.com>
2025-08-29 16:38:39 +0800

    [SCRUM-3] added UserCredentialsRepository

--- capstone/cargo-shipping-management-system/src/main/java/com/example/capstoneapplication/repository/UserCredentialsRepository.java
+++ capstone/cargo-shipping-management-system/src/main/java/com/example/capstoneapplication/repository/UserCredentialsRepository.java
@@ -3,11 +3,18 @@ package com.example.capstoneapplication.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.capstoneapplication.model.UserCredentials;
 
 @Repository
 public interface UserCredentialsRepository extends JpaRepository<UserCredentials, Long> {
-    public Optional<UserCredentials> findByUsername (String username);
+    
+    @Query(value="SELECT * FROM user_credentials WHERE username=?1 AND is_active=1 LIMIT 1", nativeQuery = true)
+    public Optional<UserCredentials> findByActiveUsername (String username);
+    public Optional<UserCredentials> findByUsernameAndPassword (String username, String password);
+
+    @Query(value="SELECT * FROM user_credentials WHERE username=?1 AND password=?2 AND is_active=1 LIMIT 1", nativeQuery = true)
+    public Optional<UserCredentials> findActiveUser (String username, String password);
 }

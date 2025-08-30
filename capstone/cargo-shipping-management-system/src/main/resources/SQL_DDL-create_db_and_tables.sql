938db65c84b43460ea2e229ff451797e92ad3db5
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2025-08-30 20:51:04 +0800

    [GTC-19] make application create database schema and tables if not yet existing

---
+++ capstone/cargo-shipping-management-system/src/main/resources/SQL_DDL-create_db_and_tables.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE IF NOT EXISTS cargo_capstone;
+USE cargo_capstone;
+
+CREATE TABLE IF NOT EXISTS `user_credentials` (`id` INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+`username` VARCHAR(255) NOT NULL UNIQUE,
+`password` VARCHAR(255) NOT NULL,
+`is_active` BOOL,
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `user_information` (`id` INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+`email` VARCHAR(255) NOT NULL UNIQUE,
+`contact_number` VARCHAR(63),
+`user_credentials_id` INTEGER NOT NULL UNIQUE,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`user_credentials_id`) REFERENCES `user_credentials`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ports` (`id` CHAR(5) NOT NULL UNIQUE,
+`name` VARCHAR(255) NOT NULL,
+`is_active` BOOL,
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `status` (`id` CHAR(2) NOT NULL UNIQUE,
+`name` VARCHAR(255) NOT NULL,
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `containers` (`id` INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+`name` VARCHAR(255) NOT NULL,
+`origin` CHAR(5) NULL,
+`destination` CHAR(5) NULL,
+`status` CHAR(2) NOT NULL,
+`weight` DOUBLE(11, 3) NULL,
+`departure_date` DATETIME NULL,
+`arrival_date` DATETIME NULL,
+`booked_by` INTEGER NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`origin`) REFERENCES `ports`(`id`),
+FOREIGN KEY (`destination`) REFERENCES `ports`(`id`),
+FOREIGN KEY (`status`) REFERENCES `status`(`id`),
+FOREIGN KEY (`booked_by`) REFERENCES `user_information`(`id`)
+);

a1eb8e1d68dde4daa01dfe1627300ac260f9080e
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2025-08-30 21:23:56 +0800

    [GTC-23] created UserService.java to handle all user-related backend logic

---
+++ capstone/cargo-shipping-management-system/src/main/java/com/example/capstoneapplication/service/UserService.java
@@ -0,0 +1,109 @@
+package com.example.capstoneapplication.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.capstoneapplication.model.UserCredential;
+import com.example.capstoneapplication.model.UserInformation;
+import com.example.capstoneapplication.repository.UserCredentialRepository;
+import com.example.capstoneapplication.repository.UserInformationRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    UserCredentialRepository userCredRepo;
+
+    @Autowired
+    UserInformationRepository userInfoRepo;
+
+    public UserService (UserCredentialRepository userCredRepo, UserInformationRepository userInfoRepo) {
+        this.userCredRepo = userCredRepo;
+        this.userInfoRepo = userInfoRepo;
+    }
+
+    public UserCredential findActiveUsername (String username) {
+        try {
+            Optional<UserCredential> foundUser = userCredRepo.findByActiveUsername (username);
+            return foundUser.get ();
+        } catch (NoSuchElementException e) {
+            return new UserCredential (-1L, "none", "none", 0);
+        }
+         catch (Exception e) {
+            e.printStackTrace ();
+            return new UserCredential (-1001L, "error", "error", 0);
+        }
+    }
+
+    public UserInformation findActiveEmail (String email) {
+        try {
+            Optional<UserInformation> foundUser = userInfoRepo.findByActiveEmail (email);
+            return foundUser.get ();
+        } catch (NoSuchElementException e) {
+            return new UserInformation (-1L, "none", "none", -1L);
+        } catch (Exception e) {
+            e.printStackTrace ();
+            return new UserInformation (-1001L, "error", "error", -1001L);
+        }
+    }
+
+    public Map<String, Object> checkIfAllowedRegister (UserCredential userCred, UserInformation userInfo) {
+        Map<String, Object> allowRegister = new HashMap<> ();
+        try {
+            UserCredential foundCredential = this.findActiveUsername (userCred.getUsername ());
+            UserInformation foundInformation = this.findActiveEmail (userInfo.getEmail ());
+
+            if (foundCredential.getId () == -1L && foundInformation.getId () == -1L) {
+                allowRegister.put ("isAllowed", true);
+            } else {
+                allowRegister.put ("isAllowed", false);
+                if (foundCredential.getId () >= 0L || foundInformation.getId () >= 0L) {
+                    allowRegister.put ("reason", "taken");
+                    allowRegister.put ("field", foundCredential.getId () >= 0L ? "username" : "email");
+                } else {
+                    allowRegister.put ("reason", "error");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace ();
+            allowRegister.put ("isAllowed", false);
+            allowRegister.put ("reason", "error");
+        }
+
+        return allowRegister;
+    }
+
+    public Map<String, Object> registerUser (UserCredential userCred, UserInformation userInfo) {
+        Map<String, Object> checkResult = this.checkIfAllowedRegister(userCred, userInfo);
+        Map<String, Object> registerResult = new HashMap<> ();
+        boolean isRegisterAllowed = (boolean) checkResult.get ("isAllowed");
+        String dbErrorMsg = "db_error";
+        try {
+            if (!isRegisterAllowed) {
+                registerResult.put ("success", false);
+                String reason = (String) checkResult.get ("reason");
+
+                if ("taken".equals (reason)) {
+                    String takenMsg = "username".equals (checkResult.get ("field")) ? "username_conflict" : "email_conflict";
+                    registerResult.put ("message", takenMsg);
+                } else if ("error".equals (reason))
+                    registerResult.put ("message", dbErrorMsg);
+            } else {
+                UserCredential createdCred = userCredRepo.save (userCred);
+                userInfo.setUserCredentialsId (createdCred.getId ());
+                userInfoRepo.save (userInfo);
+                registerResult.put ("success", true);
+                registerResult.put ("message", "user_created");
+            }
+        } catch (Exception e) {
+            registerResult.put ("success", false);
+            registerResult.put ("message", dbErrorMsg);
+        }
+
+        return registerResult;
+    }
+}
---
+++ capstone/cargo-shipping-management-system/src/test/java/com/example/capstoneapplication/service/UserServiceTest.java
@@ -0,0 +1,124 @@
+package com.example.capstoneapplication.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.capstoneapplication.model.UserCredential;
+import com.example.capstoneapplication.model.UserInformation;
+import com.example.capstoneapplication.repository.UserCredentialRepository;
+import com.example.capstoneapplication.repository.UserInformationRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    private UserCredentialRepository userCredRepo;
+    @Mock
+    private UserInformationRepository userInfoRepo;
+
+    @InjectMocks
+    private UserService userService;
+
+    private final UserCredential validUserCred = new UserCredential (1L, "test", "test_password", 1);
+    private final UserInformation validUserInfo = new UserInformation (1L, "test", "test_password", 1L);
+
+    private final UserCredential nonUserCred = new UserCredential (-1L, "none", "none", 0);
+    private final UserInformation nonUserInfo = new UserInformation (-1L, "none", "none", -1L);
+
+    private Map<String, Object> checkResultMap;
+
+    @BeforeEach
+    void setupBeforeEach () {
+        checkResultMap = new HashMap<> ();
+    }
+
+
+    @Test
+    void test_service_register_existing_username () {
+        when (userCredRepo.findByActiveUsername(any (String.class))).thenReturn (Optional.of (validUserCred));
+        when (userInfoRepo.findByActiveEmail(any (String.class))).thenReturn (Optional.empty ());
+
+        checkResultMap = userService.registerUser (validUserCred, validUserInfo);
+
+        assertFalse ((boolean)checkResultMap.get ("success"));
+        assertEquals ("username_conflict", checkResultMap.get ("message"));
+
+        verify (userCredRepo, times(0)).save (validUserCred);
+        verify (userInfoRepo, times(0)).save (validUserInfo);
+    }
+
+    @Test
+    void test_service_register_existing_email () {
+        when (userCredRepo.findByActiveUsername(any (String.class))).thenReturn (Optional.empty ());
+        when (userInfoRepo.findByActiveEmail(any (String.class))).thenReturn (Optional.of (validUserInfo));
+
+        checkResultMap = userService.registerUser (validUserCred, validUserInfo);
+
+        assertFalse ((boolean)checkResultMap.get ("success"));
+        assertEquals ("email_conflict", checkResultMap.get ("message"));
+
+        verify (userCredRepo, times(0)).save (validUserCred);
+        verify (userInfoRepo, times(0)).save (validUserInfo);
+    }
+
+    @Test
+    void test_registor_error_checking_username () {
+        when (userCredRepo.findByActiveUsername(any (String.class))).thenThrow (new RuntimeException ("DB error username check"));
+        when (userInfoRepo.findByActiveEmail(any (String.class))).thenReturn (Optional.empty ());
+
+        checkResultMap = userService.registerUser (validUserCred, validUserInfo);
+
+        assertFalse ((boolean)checkResultMap.get ("success"));
+        assertEquals ("db_error", checkResultMap.get ("message"));
+
+        verify (userCredRepo, times(0)).save (validUserCred);
+        verify (userInfoRepo, times(0)).save (validUserInfo);
+    }
+
+    @Test
+    void test_registor_error_checking_email () {
+        Map<String, Object> checkResultMap = new HashMap<> ();
+        when (userCredRepo.findByActiveUsername(any (String.class))).thenReturn (Optional.empty ());
+        when (userInfoRepo.findByActiveEmail(any (String.class))).thenThrow (new RuntimeException ("DB error email check"));
+
+        checkResultMap = userService.registerUser (validUserCred, validUserInfo);
+
+        assertFalse ((boolean)checkResultMap.get ("success"));
+        assertEquals ("db_error", checkResultMap.get ("message"));
+
+        verify (userCredRepo, times(0)).save (validUserCred);
+        verify (userInfoRepo, times(0)).save (validUserInfo);
+    }
+
+    @Test
+    void test_service_register_happy () {
+        when (userCredRepo.findByActiveUsername(any (String.class))).thenReturn (Optional.empty ());
+        when (userCredRepo.save (any (UserCredential.class))).thenReturn (validUserCred);
+        when (userInfoRepo.findByActiveEmail(any (String.class))).thenReturn (Optional.empty ());
+        when (userInfoRepo.save (any (UserInformation.class))).thenReturn (validUserInfo);
+
+        checkResultMap = userService.registerUser (validUserCred, validUserInfo);
+
+        assertTrue ((boolean)checkResultMap.get ("success"));
+        assertEquals ("user_created", checkResultMap.get ("message"));
+
+        verify (userCredRepo, times(1)).save (validUserCred);
+        verify (userInfoRepo, times(1)).save (validUserInfo);
+    }
+}

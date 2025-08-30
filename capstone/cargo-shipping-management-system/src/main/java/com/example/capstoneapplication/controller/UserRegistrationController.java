8b3865874b8af40f14ba0e17a80d90c3f9d3185e
Chiwithy <gs4var.avila@gmail.com>
2025-08-30 14:05:48 +0800

    [SCRUM-2] have started creating registration controller

--- capstone/cargo-shipping-management-system/src/main/java/com/example/capstoneapplication/controller/UserRegistrationController.java
+++ capstone/cargo-shipping-management-system/src/main/java/com/example/capstoneapplication/controller/UserRegistrationController.java
@@ -1,10 +1,16 @@
 package com.example.capstoneapplication.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.capstoneapplication.model.UserCredentials;
 import com.example.capstoneapplication.service.UserCredentialService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/register")
@@ -15,6 +21,22 @@ public class UserRegistrationController {
     public UserRegistrationController (UserCredentialService userCredService) {
         this.userCredService = userCredService;
     }
+
+    @PostMapping
+    public ResponseEntity<UserCredentials> registerUser (@RequestBody UserCredentials userCred) {
+        UserCredentials createdUser = userCredService.registerUser (userCred);
+        if (createdUser.getId () < 0L) {
+            if (createdUser.getUsername ().toLowerCase ().equals ("error"))
+                return new ResponseEntity<> (createdUser, HttpStatus.INTERNAL_SERVER_ERROR);
+            else if (createdUser.getUsername ().toLowerCase ().equals ("exist"))
+                return new ResponseEntity<> (createdUser, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<> (createdUser, HttpStatus.OK);
+    }
+    
+
+
 
 
 }
